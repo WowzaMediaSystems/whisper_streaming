18e5576a7d36301b714bd5e15ca2e8528381fa33
Ian Zenoni <ian@wowza.com>
2025-08-04 10:49:43 -0600

    cleanups

--- build_ctranslate2.sh
+++ build_ctranslate2.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+#buld the lib and whl files
 docker build -t ctranslate2:latest -f Dockerfile.ctranslate2 .
-docker run -it -v ./CTranslate2:/tmp ctranslate2:latest
+if [ $? -eq 0 ]; then
+	#copies build artifacts 
+	docker run -it -v ./CTranslate2:/tmp ctranslate2:latest
+	echo "Files copied to ./CTranslate2"
+fi
